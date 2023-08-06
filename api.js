b41453bb8ef12c38856f14fdd06e727bfe0ccdf2
Nick Saffo <122124854+PreferredOutcomes@users.noreply.github.com>
2023-08-06 18:26:54 -0400

    Add files via upload

---
+++ api.js
@@ -0,0 +1,30 @@
+// Include your existing API-related code and functions
+
+// Add an event listener when the DOM is loaded
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the Instagram authorization link element
+    const instagramLink = document.getElementById('instagramLink');
+
+    // Add a click event listener to the link
+    instagramLink.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the default link behavior
+
+        // Open Instagram authorization window
+        const clientId = 'YOUR_CLIENT_ID';
+        const redirectUri = 'YOUR_REDIRECT_URI';
+        const scope = 'user_profile,user_media';
+        const responseType = 'code';
+        const authUrl = `https://api.instagram.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}`;
+        window.location.href = authUrl;
+    });
+});
+
+// Add your existing API-related functions here
+// For example:
+function exchangeAuthorizationCode(authorizationCode) {
+    // Your code to exchange authorization code for access token using axios
+}
+
+function verifyUserFollow() {
+    // Your code to verify if the user has followed the Instagram page using the access token
+}
